94602c030e8c66cea9cd835f4e3e24042783a444
smekkley <smekkleysa@ymail.com>
2021-01-27 23:28:53 +0100

    unfuck again.

--- install-powermenu.sh
+++ install-powermenu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/bin
-cp -r rofi-blurry-powermenu/powermenu.sh ~/bin
-cp -r rofi-blurry-powermenu/powermenu_theme.rasi ~/.local/share/rofi/themes
-cp -r rofi-blurry-powermenu/fonts/* ~/.fonts/
+cp -r powermenu.sh ~/bin
+cp -r powermenu_theme.rasi ~/.local/share/rofi/themes
+cp -r fonts/* ~/.fonts/
+fc-cache -vf
