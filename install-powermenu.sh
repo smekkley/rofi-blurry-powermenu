c42d18b9cced7ca5d907ce1484aa3b0525bc36b4
smekkley <smekkleysa@ymail.com>
2021-01-27 23:17:03 +0100

    Unfuck the script1

--- install-powermenu.sh
+++ install-powermenu.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Install dependencies
-sudo apt-get install rofi scrot imagemagick -y
-
-# Copy only the neccesary files
-cp -r rofi-blurry-powermenu/powermenu.sh ~/.config/rofi
-cp -r rofi-blurry-powermenu/powermenu_theme.rasi ~/.config/rofi
-sudo cp -r rofi-blurry-powermenu/fonts/* /usr/share/fonts/
-
-# If your using Cinnamon you'll also need:
-cp -r rofi-blurry-powermenu/replace-applet.sh ~/.config/rofi
-cp -r rofi-blurry-powermenu/mod-applet/applet.js ~/.config/rofi
-cp -r rofi-blurry-powermenu/mod-applet/settings-schema.json ~/.config/rofi
+mkdir -p ~/bin
+cp -r rofi-blurry-powermenu/powermenu.sh ~/bin
+cp -r rofi-blurry-powermenu/powermenu_theme.rasi ~/.local/share/rofi/themes
+cp -r rofi-blurry-powermenu/fonts/* ~/.fonts/
